f82fe184b38eb573ec66633f8e4002660a73ce5c
Thejan-Rodrigo <thejanpandula2003@gmail.com>
2026-06-18 11:59:30 +0530

    chore: add staff id to the create appointment DTO

--- backend/SalonScheduler.API/DTOs/CreateAppointmentRequest.cs
+++ backend/SalonScheduler.API/DTOs/CreateAppointmentRequest.cs
@@ -3,5 +3,8 @@ namespace SalonScheduler.API.DTOs;
 public class CreateAppointmentRequest
 {
     public Guid ServiceId { get; set; }
+    public Guid StaffId { get; set; }
     public DateTime AppointmentDate { get; set; }
+    public DateTime StartTime { get; set; }
+    public DateTime EndTime { get; set;}
 }
